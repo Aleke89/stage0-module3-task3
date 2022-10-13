7d360ca8153ec32b837d15f8367c58de570ede22
Aleke <alnur.rysbek@mail.ru>
2022-10-13 14:47:33 +0600

    Smth-different

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,5 +3,10 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
+        int a;
+        int b;
+        int c;
+        c=10;
+        System.out.println(c);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,5 +3,14 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int first=1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first + "\n" + second +"\n"+third);
+        int linkToFirst = 15;
+        int linkToSecond = 6;
+        int linkToThird = 4;
+        System.out.println(linkToFirst + "\n" + linkToSecond +"\n"+linkToThird);
+        System.out.println(first + "\n" + second +"\n"+third);
     }
 }
